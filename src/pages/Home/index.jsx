1f8f3ffdba2bd1ff2f4d730fb91b3cd2e2a31ba4
Mzdudul <mzzdev.e@gmail.com>
2025-09-01 17:19:55 -0300

    feat: criando e consumindo dados estaticos na pagina de filmes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,43 @@
 import React from "react";
-
+import { Container, Movie, MovieList } from "./styles";
+import spiderman from "../Home/img/spiderman.webp"
 
 function Home() {
+
+    const movies = [
+        {
+            id: 1,
+            title: 'Spider Man',
+            image_url: spiderman
+        },
+        {
+            id: 2,
+            title: 'Spider Man',
+            image_url: spiderman
+        },
+        {
+            id: 3,
+            title: 'Spider Man',
+            image_url: spiderman
+        }
+
+    ]
+
     return (
-        <div>
+        <Container>
             <h1>Movies</h1>
-            <ul>
-                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
-                    <span>
-                        Spider man
-                    </span>
-                </li>
-                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
-                    <span>
-                        Spider man
-                    </span>
-                </li>
-                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
-                    <span>
-                        Spider man
-                    </span>
-                </li>
-            </ul>
-        </div>
+            <MovieList>
+                {movies.map(movie =>{
+                    return(
+                        <Movie key={movie.id}>
+                            <a href=""><img src={movie.image_url} alt={movie.title} /></a>
+                            <span>{movie.title}</span>
+                        </Movie>
+                    )
+                } )}
+                
+            </MovieList>
+        </Container>
     )
 }
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,43 @@
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+    h1{
+    text-align: center; 
+    margin: 4rem 0;
+    }
+`
+
+export const MovieList = styled.ul`
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    column-gap: 3rem;
+    row-gap: 4rem;
+
+    
+`
+
+export const Movie = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+
+    img{
+    width: 250px;
+    border-radius: 1rem;
+
+    }
+
+    span{
+    font-wheight: bold;
+    font-size: 150%;
+    margin-top: 1rem;
+    }
+
+    a:hover{
+    transform: scale(1.1);
+    transition: all 0.3s;
+    }
+`
