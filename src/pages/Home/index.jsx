6e88c6788e502eee7df7c2a0e4060f86105f01d9
Mzdudul <mzzdev.e@gmail.com>
2025-09-01 16:19:32 -0300

    chore: adicionando styled-componet

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 function Home() {
     return (
         <div>
@@ -13,7 +16,7 @@ function Home() {
                         Spider man
                     </span>
                 </li>
-                <li><img src="https://pt.wikipedia.org/wiki/Spider-Man:_No_Way_Home#/media/Ficheiro:Spider-Man_No_Way_Home_poster.jpg" alt="spider man" />
+                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
                     <span>
                         Spider man
                     </span>
