e8e79e41512be9c653784f5e602fa44c94322579
Mzdudul <mzzdev.e@gmail.com>
2025-09-01 16:13:20 -0300

    feat: criando pagina inicial

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+function Home() {
+    return (
+        <div>
+            <h1>Movies</h1>
+            <ul>
+                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
+                    <span>
+                        Spider man
+                    </span>
+                </li>
+                <li><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FSpider-Man%3A_No_Way_Home&psig=AOvVaw1bSEp8XE0JQErdMcpOaHW_&ust=1756840136143000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDLmdOhuI8DFQAAAAAdAAAAABAE" alt="spider man" />
+                    <span>
+                        Spider man
+                    </span>
+                </li>
+                <li><img src="https://pt.wikipedia.org/wiki/Spider-Man:_No_Way_Home#/media/Ficheiro:Spider-Man_No_Way_Home_poster.jpg" alt="spider man" />
+                    <span>
+                        Spider man
+                    </span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home;
