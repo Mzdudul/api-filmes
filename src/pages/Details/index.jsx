40464d1c7cf8b89d19046ab85f49560112e491e5
Mzdudul <mzzdev.e@gmail.com>
2025-09-02 19:34:43 -0300

    feat: finalizando pagina de detalhes

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,39 +1,46 @@
-
-import { use, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { API_KEY } from "../../config/key";
-import {image_path} from "../../config/img";
+import { image_path } from "../../config/img";
+import { Container } from "./styles";
 
 const Details = () => {
     const { id } = useParams();
-
-    const [movie, setMovie] = useState([]);
+    const [movie, setMovie] = useState({});
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/movie/${id}?api_key=${API_KEY}&language=EN-us&PAGE=1`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US&page=1`)
             .then((response) => response.json())
-            .then(data => {
-
-                const {title, overview, poster_path, release_date} = data;
+            .then((data) => {
+                const { title, overview, poster_path, release_date } = data;
 
                 const movie = {
                     id,
                     title,
                     sinopse: overview,
                     image: `${image_path}${poster_path}`,
-                    realeaseDate: release_date,
-                }
+                    releaseDate: release_date,
+                };
+
                 setMovie(movie);
-            })
+            });
     }, [id]);
 
-
-
-
     return (
-        <h1>Pagina de deatlhes</h1>
+        <Container>
+            <div className="movie-info">
+                <img src={movie.image} alt={movie.title} />
+                <div className="details">
+                    <h1>{movie.title}</h1>
+                    <span>Sinopse: {movie.sinopse}</span>
+                    <span className="release">Release date: {movie.releaseDate}</span>
+                    <Link to="/">
+                        <button>Go back</button>
+                    </Link>
+                </div>
+            </div>
+        </Container>
     );
-}
+};
 
 export default Details;
